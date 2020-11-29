6f64d77b031a1d59c09d54abbfcdfdd476bcb70e
Nathan Vogel <nathan@nathanvogel.com>
2020-11-29 21:34:39 +0100

    bar graph

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const width = 700;
 const height = 380;
 const dataUrl = "data.txt";
 const getDateFromYear = d3.timeParse("%Y");
+const addAYear = (date) => {
+  const nextYear = new Date(date);
+  nextYear.setFullYear(date.getFullYear() + 1);
+  return nextYear;
+};
 
 const svg = d3
   .select(".main")
@@ -9,6 +14,8 @@ const svg = d3
   .attr("preserveAspectRatio", "xMinYMin meet")
   .attr("viewBox", `0 0 ${width} ${height}`)
   .style("overflow", "visible");
+
+const graph = svg.append("g").classed("graph", true);
 
 const main = async () => {
   const temperatures = await d3.csv(dataUrl);
@@ -27,32 +34,50 @@ const main = async () => {
     .domain(tempMinMax)
     .rangeRound([height, 0])
     .nice();
+  // const colorScale = d3.scaleLinear().domain(tempMinMax).range(["blue", "red"]);
+  const colorScale = d3
+    .scaleSequential(d3.interpolateRdBu)
+    .domain([tempMinMax[1], tempMinMax[0]]);
 
-  const xaxis = d3.axisBottom(xScale);
-  const yaxis = d3
-    .axisLeft(yScale)
-    .tickFormat((temperature) => `${temperature} °C`);
-  svg
-    .append("g")
-    .classed("axis", true)
-    .attr("transform", `translate(0, ${height})`)
-    .call(xaxis);
-  svg
-    .append("g")
-    .classed("axis", true)
-    .attr("transform", `translate(0, 0)`)
-    .call(yaxis);
+  graph
+    .selectAll("rect")
+    .data(dataset)
+    .join(
+      (enter) =>
+        enter
+          .append("rect")
+          .attr("width", (d) => xScale(addAYear(d.date)) - xScale(d.date))
+          .attr("height", height)
+          .attr("fill", (d) => colorScale(d.temperature))
+          .attr("transform", (d) => `translate(${xScale(d.date)})`),
+      (update) => undefined,
+      (remove) => remove.remove()
+    );
 
-  /** @type{d3.Line<{temperature: number; date: Date;}>} */
-  const line = d3.line();
-  line.x((d) => xScale(d.date)).y((d) => yScale(d.temperature));
+  // const xaxis = d3.axisBottom(xScale);
+  // const yaxis = d3
+  //   .axisLeft(yScale)
+  //   .tickFormat((temperature) => `${temperature} °C`);
+  // svg
+  //   .append("g")
+  //   .classed("axis", true)
+  //   .attr("transform", `translate(0, ${height})`)
+  //   .call(xaxis);
+  // svg
+  //   .append("g")
+  //   .classed("axis", true)
+  //   .attr("transform", `translate(0, 0)`)
+  //   .call(yaxis);
 
-  svg
-    .append("path")
-    .datum(dataset)
-    .attr("d", line)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue");
+  // /** @type{d3.Line<{temperature: number; date: Date;}>} */
+  // const line = d3.line();
+  // line.x((d) => xScale(d.date)).y((d) => yScale(d.temperature));
+  // svg
+  //   .append("path")
+  //   .datum(dataset)
+  //   .attr("d", line)
+  //   .attr("fill", "none")
+  //   .attr("stroke", "steelblue");
 };
 
 main();
