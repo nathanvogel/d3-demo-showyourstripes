36d856c7b5758b5cae83e97ccff0533781b58f2d
Nathan Vogel <nathan@nathanvogel.com>
2020-11-29 21:19:53 +0100

    linear graph part

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+import * as d3 from "d3";
+
+interface Window {
+  d3: typeof d3;
+}
--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const height = 380;
 const dataUrl = "data.txt";
 const getDateFromYear = d3.timeParse("%Y");
 
-const svg = d3.select(".main").append("svg");
+const svg = d3
+  .select(".main")
+  .append("svg")
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", `0 0 ${width} ${height}`)
+  .style("overflow", "visible");
 
 const main = async () => {
   const temperatures = await d3.csv(dataUrl);
@@ -13,7 +18,41 @@ const main = async () => {
       temperature: parseFloat(row.temperature),
     };
   });
-  console.log(dataset);
+
+  const dateMinMax = d3.extent(dataset, (d) => d.date);
+  const tempMinMax = d3.extent(dataset, (d) => d.temperature);
+  const xScale = d3.scaleTime().domain(dateMinMax).rangeRound([0, width]);
+  const yScale = d3
+    .scaleLinear()
+    .domain(tempMinMax)
+    .rangeRound([height, 0])
+    .nice();
+
+  const xaxis = d3.axisBottom(xScale);
+  const yaxis = d3
+    .axisLeft(yScale)
+    .tickFormat((temperature) => `${temperature} °C`);
+  svg
+    .append("g")
+    .classed("axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(xaxis);
+  svg
+    .append("g")
+    .classed("axis", true)
+    .attr("transform", `translate(0, 0)`)
+    .call(yaxis);
+
+  /** @type{d3.Line<{temperature: number; date: Date;}>} */
+  const line = d3.line();
+  line.x((d) => xScale(d.date)).y((d) => yScale(d.temperature));
+
+  svg
+    .append("path")
+    .datum(dataset)
+    .attr("d", line)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue");
 };
 
 main();
